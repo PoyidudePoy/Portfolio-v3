f7f3318fdd2973690c20c51c63b2317abb6e93c6
PoyidudePoy <147290405+PoyidudePoy@users.noreply.github.com>
2026-08-03 00:13:35 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+function App() {
+  return (
+    <div className="bg-amber-0 text--gray-800">
+      <header>
+        <div className="w-full h-6 bg-gray-800 md:h-8"></div>
+      </header>
+
+      <div className="flex flex-col font-poppins font-extrabold">
+        <img src="src/assets/me.jpg" className="w-80 h-80 self-center mt-25 rounded-full border-6 border-gray-800  md:w-90 md:h-90 md:mt-48 "></img>
+        <h1 className="p-10 mt-10 text-3xl text-left items-center md:p-20 md:mt-5">Hey there, I'm <span className="text-orange-800">Clark Kent Neri.</span> student and creator. Take a look at my work, crafted with love.💓 </h1>
+      </div>
+
+
+
+
+
+    </div>
+
+
+
+  )
+
+}
+
+export default App
