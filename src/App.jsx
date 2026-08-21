442b774eae2423e422f3a3b28f027d590293f29a
PoyidudePoy <147290405+PoyidudePoy@users.noreply.github.com>
2026-08-22 00:08:54 +0800

    added hamburger
    a pretty random update

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,80 @@ import me from './assets/me.jpg'
 import custom from './assets/custom.png'
 import library from './assets/library.png'
 import guessing from './assets/guessing.png'
+import { useState } from 'react'
 
 function App() {
+  const [isNavOpen, setIsNavOpen] = useState(false)
   return (
+
     <div className="bg-beige text--gray-800">
 
       {/* this is the header*/}
 
       <header className="h-auto">
         <div className="fixed w-full h-6 bg-vintage md:h-8"></div>
-      </header>
+        <div className="fixed z-50">
 
+
+          {!isNavOpen && (
+            <nav className="block bg-white shadow md:hidden">
+              <div className="fixed top-10 right-6 z-[999999]">
+                <svg
+                  onClick={() => setIsNavOpen(prev => !prev)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="duration-200 p-1.5 bg-gray-100 hover:bg-gray-200 text-vintage h-12 w-15 rounded-2xl cursor-pointer"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.620" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </div>
+            </nav>
+          )}
+
+          {/* Mobile nav panel */}
+          <div
+            className={`fixed block md:hidden duration-300 ease bg-vintage inset-0 z-[999998] transition-transform ${isNavOpen ? 'translate-x-0' : 'translate-x-full'
+              }`}
+          >
+            <div className="absolute top-10 right-4">
+              <svg
+                onClick={() => setIsNavOpen(false)}
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-15 w-15 cursor-pointer text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                
+
+              >
+                <path className="bg-amber-50" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.620" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </div>
+
+
+            <div className="mt-30 text-center text-amber-50 font-chakra-petch">
+              <div className="text-4xl font-extrabold">
+                <a href="#projects" onClick={() => setIsNavOpen(false)} className="underline--magical">
+                  Projects
+                </a>
+              </div>
+              <div className="text-4xl mt-4 font-extrabold">
+                <a href="#random" onClick={() => setIsNavOpen(false)} className="underline--magical">
+                  Random
+                </a>
+              </div>
+              <div className="text-3xl mt-8 relative font-extrabold">
+                <a href="#contacts" onClick={() => setIsNavOpen(false)} className="border-4 px-4 py-2 border-amber-50 rounded-2xl">
+                  Contact Me
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </header>
 
       {/* this is the hero section */}
       <div className="flex flex-col font-chakra-petch md:pt-20 lg:w-12/12 lg:pl-40 lg:pr-40 lg:pt-20 lg:ml-auto mr-auto">
@@ -56,26 +119,18 @@ function App() {
       </div>
 
       {/* all my projects section */}
-      <div id="projects" className="text-xl font-chakra-petch font-extrabold p-6 bg-beige md:pt-15 md:pl-10 lg:p-20 ">
+      <div id="projects" className="text-xl font-chakra-petch font-extrabold p-6 bg-beige md:pt-12 md:pl-10 lg:p-20 ">
         <div className="text-4xl p-4 pl-0 text-left md:text-6xl">Projects🪬</div>
-        <div className="w-4/5 pb-4 md:text-4xl md:w-5/6 md:pt-2 md:pb-6 lg:pb-15">Just a collection of all the random projects i do, some are unfinished, some are in development and some are from way before! 🙆‍♂️</div>
+        <div className="w-4/5 pb-4 md:text-4xl md:w-5/6 md:pt-2 md:pb-6 lg:pb-16">My projects go here anything random 😗</div>
         <div className="grid grid-cols-12">
           <div className="col-span-12 p-4 border-5 transition hover:-translate-y-5 duration-500 border-vintage hover:shadow-xs hover:shadow-vintage duration-300 rounded-xl md:col-span-6 lg:col-span-4">
             <a href="https://poyidudepoy.github.io/Guessing-Game/">
-              <img src={guessing} className="w-20 justify-self-center"></img>
+              <img src={guessing} className="w-20 md:w-25 lg:w-30 mx-auto"></img>
               <h1 className="text-center text-3xl md:text-4xl"> Guessing Game</h1>
               <p className="text-center text-xl md:text-2xl"> a simple discrete math implication game</p>
             </a>
           </div>
-
-
         </div>
-
-
-
-
-
-
       </div>
 
 
